7e01db2ce45d003ba7e0fd026c17c252459dcdae
MJ <mjsparkflux@gmail.com>
2024-06-29 20:30:53 +0900

    Updated path

---
+++ assets/js/lottie.js
@@ -0,0 +1,7 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('animation'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: '/project-unileverkorea/assets/animations/logo_animation.json',
+})
