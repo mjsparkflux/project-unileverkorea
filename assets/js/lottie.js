9dd490ee26a8c4fbc63d6a06b9ae311b71f2aab1
MJ <mjsparkflux@gmail.com>
2024-06-29 20:17:13 +0900

    Removed unnecessary files

--- assets/js/lottie.js
+++
@@ -1,7 +0,0 @@
-var animation = bodymovin.loadAnimation({
-    container: document.getElementById('animation'), 
-    renderer: 'svg', 
-    loop: true, 
-    autoplay: true, 
-    path: '/assets/animations/logo_animation.json', 
-})
