3e72d907c0afee62f198c8b5f3ec88ea315dc437
MJ <mjsparkflux@gmail.com>
2024-07-04 22:21:39 +0900

    Removed unnecessary lines

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,38 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // var swiper = new Swiper('.slide-intro', {
-    //     // spaceBetween: 30,
-    //     centeredSlides: true,
-    //     loop: true,
-    //     autoplay: {
-    //         delay: 3000,
-    //         disableOnInteraction: false,
-    //     },
-    //     pagination: {
-    //         el: '.swiper-pagination',
-    //         type: 'fraction',
-    //     },
-    //     navigation: {
-    //         nextEl: '.swiper-button-next',
-    //         prevEl: '.swiper-button-prev',
-    //     },
-    // })
-
-    // var isPaused = false
-    // var pauseButton = document.createElement('button')
-    // pauseButton.className = 'swiper-button-pause'
-    // pauseButton.textContent = 'Pause'
-    // document.querySelector('.slide-intro').appendChild(pauseButton)
-
-    // pauseButton.addEventListener('click', function () {
-    //     if (isPaused) {
-    //         swiper.autoplay.start()
-    //         pauseButton.textContent = 'Pause'
-    //     } else {
-    //         swiper.autoplay.stop()
-    //         pauseButton.textContent = 'Play'
-    //     }
-    //     isPaused = !isPaused
-    // })
     document.addEventListener('DOMContentLoaded', function () {
         var video = document.querySelector('video')
 
